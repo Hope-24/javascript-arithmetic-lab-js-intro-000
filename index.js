ee7985255643c0363bdf4e5678b5622a020b9171
Rija Siddiqui <kakuhid@gmail.com>
2020-11-10 21:44:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function decrement(n) {
   return n-=1;
 }
 
-function makeInt(n, base){
-  return parseInt('n', 10);
+function makeInt(n){
+  parseInt('n', 10);
 }
 
 function preserveDecimal(n) {
